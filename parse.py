e543e9347e37714d7a99390795c357a89864eda9
George Thayamkery <george.thayamkery@gmail.com>
2016-05-21 21:18:07 -0500

    5/21/16 added subject detection

--- parse.py
+++ parse.py
@@ -16,7 +16,6 @@ fVerb.close()
 f_indexes = open("wordbank"+ os.sep + "verbs"+ os.sep + "index.json")
 f_letter_index = json.loads(f_indexes.read())
 f_indexes.close()
-
 def interpret(sentance):
     words = sentance.split()
     isQuestion = False
@@ -30,28 +29,42 @@ def interpret(sentance):
         isStatement = True
         isQuestion = False
 
-    ## loop through sentance
+    ## loop through sentance and assign pos
     types = []
+    count = 0
     for word in words:
-        if isverb(word):
+        if isverb(word) and count != 0:
             types.append("verb")
-        elif isadverb(word):
+        elif isadverb(word) and count != 0:
             types.append("adverb")
+        elif isconnector(word) and count != 0:
+            types.append("and_conj")
         else:
             types.append("")
-    print(types)
+        count = count + 1
 
-    # Assigning verbs to specific nouns
+
+    # finding possible subjects
     count = 0
     assigners = []
-    subject = ""
+    possible_subjects = []
+    final_subjects = []
     for t in types:
-        if t == 'verb':
-            subject = words[count-1]
-            break
+        if t == 'verb' and words[count-1] != "to":
+            if types[count-1] != "":
+                types[count] = ""
+            else:
+                possible_subjects.append(words[count-1])
+            if types[count-2] == 'and_conj':
+                possible_subjects.append(words[count-3])
         count = count + 1
-    print(subject)
+    print(types)
+    print(possible_subjects)
 
+    for w in possible_subjects:
+        i = words.index(w)
+        if types[i] == "":
+            final_subjects.append(w)
 def isverb(word):
     result = False
     first_letter = word[:1].lower()
@@ -63,6 +76,14 @@ def isverb(word):
         if item[:1].lower() != first_letter:
             break
 
+    return result
+
+def isconnector(word):
+    result = False
+    # TODO Incorporate "and" and its synonyms in the grammer.json file
+    and_synonyms = ["and", "along with", "with", "in addition to"]
+    if word in and_synonyms:
+        result = True
     return result
 
 def isadverb(word):
